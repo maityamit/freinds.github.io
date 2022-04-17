90b9167ef8fadda7d6136fef77b2cbaca2799a74
maityamit <maityamit308@gmail.com>
2022-04-17 11:18:06 +0530

    add

---
+++ Web-Dev-Angela-Yu/008/String len and no of char/JS/js.js
@@ -0,0 +1,4 @@
+var tweet="";
+tweet = prompt("Compose your tweet ! ");
+var tweetCount = tweet.length;
+alert("You have written "+ tweetCount+" charachters , you have "+(140-tweetCount)+" characters left.")
